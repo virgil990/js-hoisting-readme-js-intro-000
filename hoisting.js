7b2409240975c8917604eeeace547e3ac0d0dbce
Daniel Rubens <dan.rubens@gmail.com>
2018-05-08 01:02:26 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -10,9 +10,9 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy()();
+  thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  var thisIsCrazy() = function (){
     console.log("hey!!!")
   }
 }
